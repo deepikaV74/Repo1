8cad8b322a11d3b428f7468a6bec18cdf52a908f
deepikav74 <deepika.vellaluru@sapient.com>
2018-07-19 17:57:02 +0530

    changed sysout proj

--- csvJson/core/src/main/java/com/excel/csvJson/core/ReadExcelNCreateJson.java
+++ csvJson/core/src/main/java/com/excel/csvJson/core/ReadExcelNCreateJson.java
@@ -27,7 +27,7 @@ public class ReadExcelNCreateJson {
 			JSONArray test1 = ReadSecondExcel.getJsonFromMyExcelObject(jsonBean);
 			String jsonStr = test1.toString();
 			
-			System.out.println("marker data final json:\n " + jsonStr.substring(1, jsonStr.length()-1));
+			System.out.println("MARKER DATA final json:\n " + jsonStr.substring(1, jsonStr.length()-1));
 
 			ip.close();
 		} catch (FileNotFoundException e) {
